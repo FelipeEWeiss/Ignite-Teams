1ca9e2de1258ab547f1bfb8c32c1d80fe78f9731
FelipeWeiss <felipeew19@gmail.com>
2023-03-29 09:38:49 -0300

    improve: changed status bar

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
@@ -11,7 +11,11 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <StatusBar style="auto" />
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
       { fontsLoaded ? <Groups /> : <Loading /> }
     </ThemeProvider>
   );
