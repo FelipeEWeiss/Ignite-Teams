ea90d4af20a43bed63e87b1c6725981a5ac93d2a
FelipeWeiss <felipeew19@gmail.com>
2023-03-31 09:53:36 -0300

    improve: added flatlist to render GroupCard list

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,9 +1,16 @@
+import { useState } from 'react';
+
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
+
 import { Container } from './styles';
+import { FlatList } from 'react-native';
 
 export function Groups() {
+  const [groups, setGroups] = useState(['Rocket guys', 'Friends']);
+
+
   return (
     <Container>
       <Header />
@@ -13,8 +20,14 @@ export function Groups() {
         subtitle="Play with your team"
       />
 
-      <GroupCard
-        title="Ignite guys"
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard
+            title={item}
+          />
+        )}
       />
     </Container>
   );
