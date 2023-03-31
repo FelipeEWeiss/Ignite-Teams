2a0b6ea74ee25cbb250c7d5ec7d4ae65fd69bed8
FelipeWeiss <felipeew19@gmail.com>
2023-03-31 10:04:11 -0300

    feat: created list empty component

---
+++ src/components/ListEmpty/index.tsx
@@ -0,0 +1,13 @@
+import { Container, Message } from "./styles"
+
+interface IListEmptyProps {
+  message: string;
+}
+
+export const ListEmpty = ({ message }: IListEmptyProps) => {
+  return (
+    <Container>
+      <Message>{message}</Message>
+    </Container>
+  )
+}
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -3,12 +3,13 @@ import { useState } from 'react';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
+import { ListEmpty } from '@components/ListEmpty';
 
 import { Container } from './styles';
 import { FlatList } from 'react-native';
 
 export function Groups() {
-  const [groups, setGroups] = useState(['Rocket guys', 'Friends']);
+  const [groups, setGroups] = useState([]);
 
 
   return (
@@ -27,6 +28,10 @@ export function Groups() {
           <GroupCard
             title={item}
           />
+        )}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="How about registering first team?" />
         )}
       />
     </Container>
